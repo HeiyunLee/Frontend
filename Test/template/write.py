d2b1e5eeb25b38d395193a66b99868c91ba5860d
이혜윤 <110140094+HeiyunLee@users.noreply.github.com>
2022-10-12 16:53:47 +0900

    Delete Test directory

--- Test/template/write.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from datetime import datetime
-
-def make_test_csv(index, OimgName, imgName, imgPath, imgID, gCode):
-    new_line = pd.DataFrame([[index, OimgName, imgName, imgPath, imgID, gCode]], 
-                            columns = ["ind", "Oimg", "iN", "iP", "iID", "gC"])
-    new_line.to_csv('apply.csv', mode = "a", header = False)
